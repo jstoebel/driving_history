3a95f23dcf9f183e14f7f5a576e856a057406847
Jacob Stoebel <jstoebel@gmail.com>
2017-08-15 14:41:31 -0400

    refactor trip model

--- models/trip.rb
+++ models/trip.rb
@@ -1,5 +1,6 @@
 require 'active_record'
 require 'active_support/inflector'
+
 ##
 # represents a single trip
 # Table name: Trip
@@ -8,7 +9,6 @@ require 'active_support/inflector'
 # end_time(datetime) end time of the trip
 # drive_time(float): number of hours drive
 # miles_driven(float) the number of miles driven
-
 class Trip < ActiveRecord::Base
   belongs_to :driver
 
@@ -26,16 +26,23 @@ class Trip < ActiveRecord::Base
             presence: true,
             numericality: { greater_than: 0 }
 
-  # compute the drive_time if the record passes validation (needs to have a start and end time)
-  after_validation :get_drive_time, if: proc { |t| t.errors.none? }
+  # compute the drive_time if the record passes validation
+  # (needs to have a start and end time)
+  after_validation :compute_drive_time, if: proc { |t| t.errors.none? }
+  after_validation :validate_avg_speed, if: proc { |t| t.errors.none? }
 
   private
 
   ##
   # computes the drive time of the trip
+  def compute_drive_time
+    self.drive_time = (end_time - start_time) / 3600 # convert seconds to hours
+  end
+
+  ##
+  # validates average speed for trip.
   # if the average speed is less than 5 or more than 100 its not valid
-  def get_drive_time
-    self.drive_time = (end_time - start_time) / 3600 # compute drive time
+  def validate_avg_speed
     avg_speed =  miles_driven / drive_time # compute average_speed
 
     # add an errors if < 5 or > 100 mph
