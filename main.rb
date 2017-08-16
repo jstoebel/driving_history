8848f58d6bb4232f88514c6d774e0f9b6243cbed
Jacob Stoebel <jstoebel@gmail.com>
2017-08-16 13:49:54 -0400

    clean up for main

--- main.rb
+++ main.rb
@@ -5,8 +5,9 @@ Bundler.require(:default, :development)
 require './command_file'
 require './schema'
 
-file_loc = ARGV[0]
+file_loc = ARGV[0] # get file path from command line argument
 
+# process the data and report the results
 command_file = CommandFile.new file_loc
 command_file.process
 command_file.report
